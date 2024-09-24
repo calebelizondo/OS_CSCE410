0c40b418b762cdc7559992afae4b95901c8bfe79
celizondo <calebelizondo@tamu.edu>
2024-09-24 14:14:24 -0500

    moved cont_frame_pool to new MP

--- MP3/cont_frame_pool.C
+++ MP3/cont_frame_pool.C
@@ -1,8 +1,8 @@
 /*
  File: ContFramePool.C
  
- Author:
- Date  : 
+ Author: Caleb Elizondo
+ Date  : 9/15/2023
  
  */
 
@@ -102,6 +102,7 @@
 
 #include "cont_frame_pool.H"
 #include "console.H"
+#include "utils.H"
 #include "assert.H"
 
 /*--------------------------------------------------------------------------*/
@@ -114,7 +115,10 @@
 /* CONSTANTS */
 /*--------------------------------------------------------------------------*/
 
-/* -- (none) -- */
+
+const unsigned int ContFramePool::maxPools = 2;
+unsigned int ContFramePool::npools = 0;
+ContFramePool* ContFramePool::pools[ContFramePool::maxPools];
 
 /*--------------------------------------------------------------------------*/
 /* FORWARDS */
@@ -128,40 +132,155 @@
 
 ContFramePool::ContFramePool(unsigned long _base_frame_no,
                              unsigned long _n_frames,
-                             unsigned long _info_frame_no)
+                             unsigned long _info_frame_no) : 
+    base_frame_no(_base_frame_no), nframes(_n_frames), info_frame_no(_info_frame_no), nFreeFrames(_n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    Console::puts("ContframePool::Constructor not implemented!\n");
-    assert(false);
+    assert(_n_frames <= FRAME_SIZE * 2);
+    assert(npools != maxPools);
+
+    n_info_frames = needed_info_frames(nframes);
+
+    //allocate bitmap
+    if (info_frame_no == 0) bitmap = (unsigned char *) (base_frame_no * FRAME_SIZE);
+    else bitmap = (unsigned char *) (info_frame_no * FRAME_SIZE);
+
+    //initialize frames to free state
+    for (int fno = 0; fno < _n_frames; fno++) set_state(fno, FrameState::Free);
+    
+    //allocate info frames
+    if (_info_frame_no == 0) {
+
+        if (n_info_frames <= 1) {
+            set_state(0, FrameState::HoS);
+            nFreeFrames--;
+        }
+        else {
+            mark_inaccessible(0, n_info_frames);
+        }
+    }
+
+    //keep track of the current amount of frame pools
+    pools[npools] = this;
+    npools++;
+
+    Console::puts("Frame pool initialized! \n");
 }
 
 unsigned long ContFramePool::get_frames(unsigned int _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    Console::puts("ContframePool::get_frames not implemented!\n");
-    assert(false);
-    return 0; // keep compiler happy
+    assert(nFreeFrames >= _n_frames);
+
+    unsigned int fno = base_frame_no;
+    unsigned int seq_start = 0;
+
+    //while we haven't reached the end of the bitmap ...
+    while (seq_start <= nframes - _n_frames) {
+
+        //identify the next available free frame
+        while (seq_start < nframes && get_state(seq_start) != FrameState::Free) 
+            seq_start++;
+
+        unsigned int search_frame = seq_start;
+        unsigned int seq_length = 0;
+        //see how long the sequence goes for ...
+        while (search_frame < nframes && get_state(search_frame) == FrameState::Free && seq_length < _n_frames) {
+            search_frame++;
+            seq_length++;
+        }
+        //if the sequence is big enough, allocate the space
+        if (seq_length == _n_frames) {
+            fno += seq_start;
+            mark_inaccessible(fno, _n_frames);
+            return fno;
+        }
+
+        if (search_frame >= nframes) return 0;
+    }
+
+    return 0;
 }
 
 void ContFramePool::mark_inaccessible(unsigned long _base_frame_no,
                                       unsigned long _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    Console::puts("ContframePool::mark_inaccessible not implemented!\n");
-    assert(false);
+    unsigned int base_index = _base_frame_no - base_frame_no;
+    assert(get_state(base_index) == FrameState::Free);
+
+    //iterate from base to end frame, mark each as used
+    set_state(base_index, FrameState::HoS);
+	for (int fno = base_index + 1; fno < base_index + _n_frames; fno++) {
+        assert(get_state(fno) == FrameState::Free);
+        set_state(fno, FrameState::Used);
+	}
+	
+	nFreeFrames -= _n_frames;
+
 }
+
+void ContFramePool::_release_frames(unsigned long _first_frame_no)
+{
+
+    set_state(_first_frame_no, FrameState::Free);
+    nFreeFrames += 1;
+    //iterate from start to end of sequence, free frames
+    unsigned long fno = _first_frame_no + 1;
+    while (fno < base_frame_no + nframes && get_state(fno) == FrameState::Used) {
+        set_state(fno, FrameState::Free);
+        nFreeFrames += 1;
+    }
+}
+
 
 void ContFramePool::release_frames(unsigned long _first_frame_no)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    Console::puts("ContframePool::release_frames not implemented!\n");
-    assert(false);
+
+    //for each frame pool ...
+    for (ContFramePool* pool : pools) {
+        //if the frame is within the pool's bounds
+        if (_first_frame_no >= pool->base_frame_no && 
+            _first_frame_no < pool->base_frame_no + pool->nframes) {
+            //release the frames
+            pool->_release_frames(_first_frame_no);
+            break;
+        }
+    }
+
+    
 }
 
 unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    Console::puts("ContframePool::need_info_frames not implemented!\n");
-    assert(false);
-    return 0; // keep compiler happy
+	return (_n_frames) / FRAME_SIZE + (_n_frames % FRAME_SIZE > 0 ? 1 : 0);
+}
+
+ContFramePool::FrameState ContFramePool::get_state(unsigned long _frame_no) {        
+
+    switch (bitmap[_frame_no]) {
+        case 0:
+            return FrameState::Used;
+        case 1:
+            return FrameState::Free;
+        case 2:
+            return FrameState::HoS;
+    }
+
+    return FrameState::Used;
+}
+
+void ContFramePool::set_state(unsigned long _frame_no, FrameState _state) {
+
+    unsigned char new_state_bits = 0;
+    switch (_state) {
+        case FrameState::Used:
+            new_state_bits = 0;
+            break;
+        case FrameState::Free:
+            new_state_bits = 1;
+            break;
+        case FrameState::HoS:
+            new_state_bits = 2;
+            break;
+    }
+
+    bitmap[_frame_no] = new_state_bits;
 }
