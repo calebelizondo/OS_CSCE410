252265dc7a936d7d89974ad1ce7e0533c59b4b3a
celizondo <calebelizondo@tamu.edu>
2024-09-26 17:21:13 -0500

    finished constructor

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -26,7 +26,28 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
 
 PageTable::PageTable()
 {
-   assert(false);
+   //request first frame to store directory
+   this->page_directory = (unsigned long *) (kernel_mem_pool->get_frames(1) * PAGE_SIZE);
+   
+   //request frame to store table
+   unsigned long * page_table = (unsigned long *) (kernel_mem_pool->get_frames(1) * PAGE_SIZE);
+   
+   unsigned long address = 0; 
+   unsigned long shared_frames_n = PageTable::shared_size / PAGE_SIZE;
+
+   for (int i = 0; i < shared_frames_n; i++){
+      page_table[i] = address | PAGE_WRITE | PAGE_PRESENT;
+      address += PAGE_SIZE;
+   }
+
+   page_directory[0] = (unsigned long) page_table | PAGE_WRITE | PAGE_PRESENT;
+
+   address = 0;
+
+   for (int i = 1; i < shared_frames_n; i++) {
+      page_directory[i] = address | PAGE_WRITE;
+   }
+
    Console::puts("Constructed Page Table object\n");
 }
 
@@ -44,7 +65,7 @@ void PageTable::enable_paging()
 {
    //assert(false);
    PageTable::paging_enabled = 1;
-
+   write_cr0(read_cr0() | 0x80000000);
    Console::puts("Enabled paging\n");
 }
 
